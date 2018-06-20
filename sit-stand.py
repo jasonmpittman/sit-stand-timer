f87c106c2e28114b955b778289f6c2e8ff0f7033
jasonmpittman <jmpittman@captechu.edu>
2018-06-20 07:57:15 -0400

    initial commit

---
+++ sit-stand.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Created on 06/19/2018
+# @author: Jason M. Pittman
+# @license: MIT-license
+# Purpose: a sit stand move timer for notifications at a standing desk
+# Explanation: 
+import time
+
+class StateTimer():
+    def __init__(self):
+        self.state_timer = time.time() + 2700
+    
+    def get_timer(self):
+        return self.state_timer
+    
+    def set_timer(self, new_time):
+        self.state_timer = new_time
+
+class State():
+    def __init__(self):
+        self.state = "Sit"
+        self.st = StateTimer()
+    
+    def change_state(self, new_state):
+        self.state = new_state
+
+        if self.state == "Stand":
+            self.st = time.time() + 600
+            return self.st
+
+        if self.state == "Sit":
+            self.st = time.time() + 2700
+            return self.st
+        
+        if self.state == "Move":
+            self.st == time.time() + 240
+            return self.st
+
+def main():
+    st = StateTimer()
+    s = State()
